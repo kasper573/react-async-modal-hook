05c04b8139259f3b8df1a7e61c3737e5f55b1e3f
Kasper <805006+kasper573@users.noreply.github.com>
2025-12-16 22:21:36 +0100

    add strict mode to playwright test cases

--- apps/playwright/src/create-test-case.tsx
+++ apps/playwright/src/create-test-case.tsx
@@ -1,12 +1,14 @@
-import type { ComponentType } from "react";
+import { StrictMode, type ComponentType } from "react";
 import { ModalStore, ModalContext, ModalOutlet } from "react-async-modal-hook";
 
 export function createTestCase(Content: ComponentType): ComponentType {
   const store = new ModalStore();
   return () => (
-    <ModalContext.Provider value={store}>
-      <Content />
-      <ModalOutlet />
-    </ModalContext.Provider>
+    <StrictMode>
+      <ModalContext.Provider value={store}>
+        <Content />
+        <ModalOutlet />
+      </ModalContext.Provider>
+    </StrictMode>
   );
 }
